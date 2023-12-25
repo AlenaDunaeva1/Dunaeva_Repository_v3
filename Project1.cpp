c89dc4eb195c09bfb08d04ee97e9a43797b6140c
Alena Dunaeva <dunalalex1@gmail.com>
2023-12-25 14:12:20 +0300

    create progect

---
+++ Project1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int EnterNumber(int num) {
+
+	return 0;
+}
+
+void CalcArea(int num) {
+
+}
+
+void CalcPerim(int num) {
+
+}
+
+int main()
+{
+    int x;
+    std::cout<<"1. Enter a positive number A \n2. Calculate the area of a triangle \n3. Calculate the perimeter of a triangle "<< std::endl;
+    std::cin >> x;
+
+	switch (x)
+	{
+	case 1:
+		EnterNumber(x);
+		break;
+	case 2:
+		CalcArea(x);
+		break;
+	case 3:
+		CalcPerim(x);
+		break;
+	default:
+		break;
+	}
+}
