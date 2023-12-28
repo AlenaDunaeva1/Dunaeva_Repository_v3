b16bf0254e13ebb95f381ea237b0c00ab54e3d0c
Dunaeva Alena <dunalalex1@gmail.com>
2023-12-28 20:05:26 +0300

    final commit

--- Project1.cpp
+++ Project1.cpp
@@ -1,36 +1,41 @@
 ﻿#include <iostream>
+#include <cmath>
 
-int EnterNumber(int num) {
+int n;
 
-	return 0;
+int EnterNumber() {
+	std::cout << "Enter lenght: " << std::endl;
+	std::cin >> n;
+	return n;
 }
 
-void CalcArea(int num) {
-
+void CalcArea() {
+	float S = pow(n, 2) * sqrt(3) / 4;
+	std::cout << " Area: " << S << std::endl;
 }
 
-void CalcPerim(int num) {
-
+void CalcPerim() {
+	int P = n * 3;
+	std::cout << " Perimeter: " << P << std::endl;
 }
 
 int main()
 {
-    int x;
-    std::cout<<"1. Enter a positive number A \n2. Calculate the area of a triangle \n3. Calculate the perimeter of a triangle "<< std::endl;
-    std::cin >> x;
+	int x;
+	EnterNumber();
 
+	std::cout << "Choose an action:\n 1. Calculate the area of a triangle \n 2. Calculate the perimeter of a triangle " << std::endl;
+	std::cin >> x;
 	switch (x)
 	{
 	case 1:
-		EnterNumber(x);
+		CalcArea();
 		break;
 	case 2:
-		CalcArea(x);
-		break;
-	case 3:
-		CalcPerim(x);
+		CalcPerim();
 		break;
 	default:
+		std::cout << "Error" << std::endl;
 		break;
 	}
 }
